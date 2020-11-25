9a97ef6e3df9c57ff37d2140d1f286822ab36f22
_ Trentini _ <pietrotrentini5@gmail.com>
2020-11-25 13:30:15 +0100

    Add project files.

---
+++ Prova/Program.cs
@@ -0,0 +1,18 @@
+﻿using metodi;
+using System;
+
+namespace Prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double n1 = Metodo.InserisciLato();
+            double n2 = Metodo.InserisciLato();
+            double n3 = Metodo.InserisciLato();
+            Metodo.Classificazione(n1, n2, n3);
+            Console.WriteLine($"Il suo perimetro è {Metodo.Perimetro(n1, n2, n3)}");
+        }
+    }
+}
---
+++ metodi/Metodo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace metodi
+{
+    public class Metodo
+
+    {
+        public static double InserisciLato()
+        {
+
+
+            Console.WriteLine("Inserisci un Lato");
+            double n = int.Parse(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("Lato Inserito non Corretto");
+                Console.WriteLine("Inserire un Lato Valido");
+                double n1 = double.Parse(Console.ReadLine());
+                return n1;
+            }
+            return n;
+
+
+        }
+        public static void Classificazione(double n1, double n2, double n3)
+        {
+            if (n1 != n2 && n1 != n3 && n2 != n3)
+            {
+                Console.WriteLine("Triangolo Scaleno");
+            }         
+            else if (n1 != n2 || n1 != n3 || n2 != n3)
+            {
+                Console.WriteLine("Triangolo Isoscele");
+            }
+            else if (n1 <= n2 && n1 <= n3 && n2 <= n3)
+            {
+                Console.WriteLine("Triangolo Equilatero");
+            }
+        }
+        public static double Perimetro(double n1, double n2, double n3)
+        {
+            double p = n1 + n2 + n3;
+            return p;
+        }
+    }
+}
+
+
+
+
