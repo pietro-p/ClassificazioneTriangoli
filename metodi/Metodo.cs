5af769810fc6f6b89285b7d5d8df7b26ed98680c
_ Trentini _ <pietrotrentini5@gmail.com>
2020-11-25 21:46:25 +0100

    modifica1

--- metodi/Metodo.cs
+++ metodi/Metodo.cs
@@ -7,27 +7,20 @@ namespace metodi
     {
         public static double InserisciLato()
         {
-
-
-            Console.WriteLine("Inserisci un Lato");
-            double n = int.Parse(Console.ReadLine());
-            if (n <= 0)
+            double n;
+            do
             {
-                Console.WriteLine("Lato Inserito non Corretto");
-                Console.WriteLine("Inserire un Lato Valido");
-                double n1 = double.Parse(Console.ReadLine());
-                return n1;
-            }
+                Console.WriteLine("inserisci un lato: ");
+                n = Convert.ToDouble(Console.ReadLine());
+            } while (n < 0);
             return n;
-
-
         }
         public static void Classificazione(double n1, double n2, double n3)
         {
             if (n1 != n2 && n1 != n3 && n2 != n3)
             {
                 Console.WriteLine("Triangolo Scaleno");
-            }         
+            }
             else if (n1 != n2 || n1 != n3 || n2 != n3)
             {
                 Console.WriteLine("Triangolo Isoscele");
